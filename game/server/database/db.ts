c5753b39a08d518ea930df4eff6c7c9795941d29
Christopher <onlygamernerd@gmail.com>
2021-06-27 20:42:46 +0200

    feat(server/db): initial database setup

---
+++ game/server/database/db.ts
@@ -0,0 +1,11 @@
+
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: parseInt(process.env.DB_PORT) || 3306,
+  database: process.env.DB_NAME || 'battlefield',
+});
+
+export const db = pool.promise();
