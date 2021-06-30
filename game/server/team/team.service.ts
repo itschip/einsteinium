877c5464921b58cf75d65fae5e19c5baf103c5f7
Christopher <onlygamernerd@gmail.com>
2021-06-30 02:21:16 +0200

    feat(server/team): inital team setup

--- game/server/team/team.service.ts
+++ game/server/team/team.service.ts
@@ -1,25 +1,28 @@
-import { TeamProps } from "./team.interface";
-import PlayerService from "../player/player.service";
+import { TeamProps } from './team.interface';
+import PlayerService from '../player/player.service';
 
 class _TeamService {
-	private readonly redTeamMap: Map<number, TeamProps>;
-	private readonly blueTeamMap: Map<number, TeamProps>;
+  private redTeamAmount: number;
+  private blueTeamAmount: number;
 
-	constructor() {
-		console.log('Team service started');
-		this.blueTeamMap = new Map<number, TeamProps>()
-		this.redTeamMap = new Map<number, TeamProps>()
-	}
+  constructor() {
+    console.log('Team service started');
+    this.blueTeamAmount = 0;
+    this.redTeamAmount = 0;
+  }
 
-	 handleAssignTeam(src: number) {
-		const Player = PlayerService.getPlayer(src);
-		this.blueTeamMap.set(src, { username: Player.getUsername(), deaths: 0, kills: 0 });
+  get redTeam() {
+    return this.redTeamAmount;
+  }
 
-		const player = this.blueTeamMap.get(src);
+  get blueTeam() {
+    return this.blueTeamAmount;
+  }
 
-		console.log(this.blueTeamMap.get(src));
-	 }
-
+  addPlayerToTeam(team: 'red' | 'blue') {
+    if (team === 'red') this.redTeamAmount = this.redTeamAmount + 1;
+    if (team === 'blue') this.blueTeamAmount = this.blueTeamAmount + 1;
+  }
 }
 
 const TeamService = new _TeamService();
