76a3b223ca3cd724ce0fc60a3f62b6805a0703f4
Christopher <onlygamernerd@gmail.com>
2021-06-30 02:23:28 +0200

    feat(core): update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,12 @@
 fx_version "cerulean"
 game "gta5"
 
-client_script "game/dist/client/*.js"
+client_script {
+	"game/client/**/*.lua",
+	"game/dist/client/*.js"
+}
 server_script "game/dist/server/*.js"
+
+files {
+	"game/shared/config.json"
+}
