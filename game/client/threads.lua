f7b76712b626fd28ae96d33c916d74d3773a0f1a
Neco <13007790+The-Neco@users.noreply.github.com>
2021-07-01 13:25:41 +0100

    feat(client/threads): Minor optimisations.

--- game/client/threads.lua
+++ game/client/threads.lua
@@ -1,28 +1,40 @@
+plyData = {}
+
+Citizen.CreateThread(function()
+	while true do
+		plyData.ped = PlayerPedId()
+		plyData.playerId = PlayerId()
+		playerData.coords = GetEntityCoords(plyData.ped)
+		playerData.health = GetEntityHealth(plyData.ped)
+		Wait(100)
+	end
+end)
+
 CreateThread(function()
 	ClearPlayerWantedLevel(PlayerId());
 	SetMaxWantedLevel(0)
 end)
 
+local prLog = GetZoneFromNameId("PrLog")
 
 local isAimingDownSights = false
 CreateThread(function()
-	
 	while true do
 		Wait(0)
 		
-		RestorePlayerStamina(PlayerId(), 1.0);
-		SetZoneEnabled(GetZoneFromNameId("PrLog"), false) -- Removes Snowflakes from Cayo Perico
+		RestorePlayerStamina(plyData.playerId, 1.0);
+		SetZoneEnabled(prLog, false) -- Removes Snowflakes from Cayo Perico
 		
-		if IsPedOnFoot(PlayerPedId()) then
+		if IsPedOnFoot(plyData.ped) then
 			SetFollowPedCamViewMode(4)
 			HideHudComponentThisFrame(14)
 			-- Disables V
-			DisableControlAction(0, 0, true);
-			DisableControlAction(1, 0, true);
-			DisableControlAction(2, 0, true);
+			DisableControlAction(0, 0, true)
+			DisableControlAction(1, 0, true)
+			DisableControlAction(2, 0, true)
 		end
 		
-		if IsPlayerFreeAiming(PlayerId()) then
+		if IsPlayerFreeAiming(plyData.playerId) then
 			if not isAimingDownSights then
 				SetControlNormal(0, 50, 1.0)
 				isAimingDownSights = true
